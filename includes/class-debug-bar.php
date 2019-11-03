6348c3048dcf463890359123d2d617105a10f886
Zorca Orcinus <vs@zorca.org>
2019-11-03 19:19:36 +0300

    Collectors added

--- includes/class-debug-bar.php
+++ includes/class-debug-bar.php
@@ -8,6 +8,10 @@
 namespace Wpseed\Indabug;
 
 use DebugBar\DebugBar;
+use DebugBar\DataCollector\PhpInfoCollector;
+use DebugBar\DataCollector\MessagesCollector;
+use DebugBar\DataCollector\MemoryCollector;
+use DebugBar\DataCollector\ExceptionsCollector;
 
 /**
  * Class Debug_Bar
@@ -16,4 +20,15 @@ use DebugBar\DebugBar;
  */
 class Debug_Bar extends DebugBar {
 
+	/**
+	 * Debug_Bar constructor.
+	 *
+	 * @throws \DebugBar\DebugBarException DebugBar Exception.
+	 */
+	public function __construct() {
+		$this->addCollector( new PhpInfoCollector() );
+		$this->addCollector( new MessagesCollector() );
+		$this->addCollector( new MemoryCollector() );
+		$this->addCollector( new ExceptionsCollector() );
+	}
 }
